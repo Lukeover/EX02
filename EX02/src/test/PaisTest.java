8c9d70b5896a6597d07055960402afae09c21d80
Lukas Barbosa de Oliveira <36937245+Lukeover@users.noreply.github.com>
2018-03-13 17:26:04 -0300

    Add files via upload

---
+++ EX02/src/test/PaisTest.java
@@ -0,0 +1,89 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.FixMethodOrder;
+import org.junit.Test;
+import org.junit.runners.MethodSorters;
+
+import model.Pais;
+
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+public class PaisTest {
+
+	Pais pais, copia;
+	static int id = 0;
+
+	/*
+	 * Antes de rodar este teste, certifique-se que nao ha no banco nenhuma
+	 * linha com o id igual ao do escolhido para o to instanciado abaixo. Se
+	 * houver, delete. 
+	 * Certifique-se também que sobrecarregou o equals na classe
+	 * Cliente. 
+	 * Certifique-se que a fixture cliente1 foi criada no banco.
+	 * Além disso, a ordem de execução dos testes é importante; por
+	 * isso a anotação FixMethodOrder logo acima do nome desta classe
+	 */
+	@Before
+	public void setUp() throws Exception {
+		System.out.println("setup");
+		pais = new Pais(id, "moacir", 27192, 312123 );
+		copia = new Pais(id, "moacir", 27192, 312123 );
+		System.out.println(pais);
+		System.out.println(copia);
+		System.out.println(id);
+	}
+	
+	@Test
+	public void test00Carregar() {
+		System.out.println("carregar");
+		//para funcionar o cliente 1 deve ter sido carregado no banco por fora
+		Pais fixture = new Pais(1,"Brasil",2077000000,8516000);
+		Pais novo = new Pais(1, null, -1, -1);
+		novo.carregar();
+		assertEquals("testa inclusao", novo, fixture);
+	}
+
+	@Test
+	public void test01Criar() {
+		System.out.println("criar");
+		pais.criar();
+		id = pais.getId();
+		System.out.println(id);
+		copia.setId(id);
+		assertEquals("testa criacao", pais, copia);
+	}
+
+	@Test
+	public void test02Atualizar() {
+		System.out.println("atualizar");
+		pais.setPopulacao(886427);
+		copia.setPopulacao(886427);		
+		pais.atualizar();
+		pais.carregar();
+		assertEquals("testa atualizacao", pais, copia);
+	}
+
+	@Test
+	public void test03Excluir() {
+		System.out.println("excluir");
+		copia.setId(-1);
+		copia.setNome(null);
+		copia.setPopulacao(-1);
+		copia.setArea(-1);
+		pais.excluir();
+		pais.carregar();
+		assertEquals("testa exclusao", pais, copia);
+	}
+
+	
+	@Test
+	public void test04Menor() {
+		System.out.println(pais.menorArea());
+	}
+	@Test
+	public void test05Maior() {
+		System.out.println(pais.maiorPopulacao());
+	}
+}
